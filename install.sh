1e3eade1e78541b5d6b830c9aa68fd6b77bebbef
pandineer <pandineer@gmail.com>
2025-12-18 18:20:45 +0900

    karabinerの設定を追加

--- install.sh
+++ install.sh
@@ -13,6 +13,31 @@ for dotfile in .??*
 do
     [[ "$dotfile" == ".git" ]] && continue       # ignore .git
     [[ "$dotfile" == ".gitignore" ]] && continue # ignore .gitignore
+
+    # ---- special handling for .config ----
+    if [[ "$dotfile" == ".config" && -d "$dotfile" ]]; then
+	mkdir -p ~/.config
+
+	for config_item in "$pwd/.config/"*
+	do
+	    name=$(basename "$config_item")
+	    target="$HOME/.config/$name"
+
+	    if [ -e "$target" ] || [ -L "$target" ]; then
+		read -p "$target already exists. overwrite? [Y/n] " answer
+		case $answer in
+		    [Nn]* ) continue ;;
+		esac
+	    fi
+
+	    # file / directory の区別なく symlink
+	    ln -sfn "$config_item" "$target"
+	done
+
+	continue
+    fi
+    # -------------------------------------
+
     if [ -e ~/$dotfile ]; then # If file or directory already exist. 
         if [ -d ~/$dotfile ]; then # If directory already exists, "ln" command could not overwrite it. 
             read -p "$dotfile directory already exists. Please remove or rename it and run $0 again. Continue? [Y/n]" answer
